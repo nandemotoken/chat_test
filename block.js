80d303028cffffe49c25c96342595d58f9094bd6
nandemotoken <nandemotoken@gmail.com>
2020-06-20 03:22:20 +0000

    create block.js

--- block.js
+++ block.js
@@ -4,8 +4,8 @@ const web3p = new Web3(portis.provider);
 const Address =  "0x2Ddef8aB2d2f569ab67845075805e6725095e2BA";
 // console.log(Address);
 const account = await web3p.eth.getAccounts();
-console.log(accounts);
+console.log(account);
 mycontract = await new web3p.eth.Contract(ABI, Address);
-mes = await mycontract.methods.balanceOf(accounts).call();
+mes = await mycontract.methods.balanceOf(account).call();
 console.log(mes);  
 }
