404ef5ce004e3c2d728c61a72fce3cff08402a56
nandemotoken <nandemotoken@gmail.com>
2020-06-20 02:04:58 +0000

    create block.js

--- block.js
+++ block.js
@@ -1,5 +1,7 @@
 window.onload = () =>{
 const portis = new Portis('5716c193-5951-43ca-9156-5d53a258cb7b', 'ropsten');
 const web3 = new Web3(portis.provider);
-
+web3.eth.getAccounts((error, accounts) => {
+    console.log(accounts);
+  });
 }
