20ed068c534444feb730954f4e313734c5b55efd
nandemotoken <nandemotoken@gmail.com>
2020-06-20 03:09:29 +0000

    create block.js

--- block.js
+++ block.js
@@ -1,7 +1,10 @@
-window.onload = () =>{
+window.onload = async () =>{
 const portis = new Portis('5716c193-5951-43ca-9156-5d53a258cb7b', 'ropsten', { gasRelay: true });
-const web3 = new Web3(portis.provider);
-web3.eth.getAccounts((error, accounts) => {
+const web3p = new Web3(portis.provider);
+const Address =  "0x2Ddef8aB2d2f569ab67845075805e6725095e2BA"
+await web3p.eth.getAccounts((error, accounts) => {
     console.log(accounts);
+    mycontract = await new web3p.eth.Contract(ABI, Address);
+    mes = await mycontract.methods.balanceOf(accounts).call()  
   });
 }
